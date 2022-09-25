97f743f528986c935dca6f6d34755bf897aa1229
Grigoriy <foxtail25@gmail.com>
2022-09-26 00:11:46 +0300

    сделал смену цвета

--- js/js.js
+++ js/js.js
@@ -15,7 +15,8 @@ let sum = num1 + num2;
 let input = document.getElementById('answer');
 // Доступ к месту где отображается ответ
 let answ = document.querySelector('.check');
-
+// доступ к фону задач (основному блоку)
+let main = document.querySelector('.main')
 function new_task () {
     let num1 = getRandomInRange(0, 100);
     let num2 = getRandomInRange(0, 100);
@@ -25,6 +26,8 @@ function new_task () {
     task.innerText = randomTaskText;
     answ.innerText = '';
     input.value = '';
+    input.style.backgroundColor = '#FFF';
+    main.style.backgroundColor = 'rgba(255,255,255,0.65';
 }
 new_task()
 
@@ -44,11 +47,15 @@ function check() {
 function answer (a) {
     if (a === "right") {
         answ.innerText = `Правильно ${sum}`
+        input.style.backgroundColor = '#60F52F';
+        main.style.backgroundColor = 'rgba(178,217,134,0.65';
     } else if (a === 'empty') {
         answ.innerText = `Вы не ввели ответ`
         
     } else if (a === "wrong") {
         answ.innerText = `К сожалению ваш ответ не верный.`
+        main.style.backgroundColor = 'rgba(250,105,37,0.65';
+        input.style.backgroundColor = 'red';
     } else {
     answ.innerText = `Ответ ${sum}`;
     }
